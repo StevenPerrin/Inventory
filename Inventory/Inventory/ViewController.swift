a30fa5a726fd820f2f5b9986254f4989743553b3
Steven Perrin <spcnn@mail.missouri.edu>
2019-11-08 13:42:08 -0600

    Initial commit

---
+++ Inventory/Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Steven Perrin on 11/8/19.
+//  Copyright © 2019 Steven Perrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
